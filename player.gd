3c575a2242794609ecf704393ed9aa2fc793359b
Lily <66401081+Lilium-Github@users.noreply.github.com>
2025-10-09 13:10:41 -0600

    Add files via upload

---
+++ player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+const speed = 100
+
+@onready var animatedSprite = $AnimatedSprite2D
+@onready var interactUI = $InteractUI
+@onready var inventoryUI = $InventoryUI
+
+func _ready() -> void:
+	GlobalInventory.setPlayerNode(self)
+
+func getInput():
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if direction and !inventoryUI.visible:
+		self.velocity = direction * speed
+	else:
+		self.velocity = Vector2.ZERO
+
+func _physics_process(_delta: float) -> void:
+	getInput()
+	move_and_slide()
+	updateAnimations()
+
+func updateAnimations():
+	if velocity == Vector2.ZERO:
+		animatedSprite.play("idle")
+	else:
+		if abs(velocity.x) > abs(velocity.y):
+			if velocity.x > 0:
+				animatedSprite.play("walkRight")
+			else:
+				animatedSprite.play("walkLeft")
+		else:
+			if velocity.y > 0:
+				animatedSprite.play("walkDown")
+			else:
+				animatedSprite.play("walkUp")
+
+func _input(event):
+	if event.is_action_pressed("InventoryUI"):
+		inventoryUI.visible = !inventoryUI.visible
+		get_tree().paused = !get_tree().paused
+		
